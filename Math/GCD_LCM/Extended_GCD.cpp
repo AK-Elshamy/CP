8fd488f7f9c6c6f8923a2f58023a003690dbe637
Ahmed Elshamy <ahmed.khalid.elshamy37@gmail.com>
2025-08-27 14:23:46 +0300

    add new topic for math

---
+++ Math/GCD_LCM/Extended_GCD.cpp
@@ -0,0 +1,14 @@
+
+// Extended Euclidean Algorithm
+// Finds x, y such that a*x + b*y = gcd(a, b) 
+long long extgcd(long long a, long long b, long long &x, long long &y) {
+    if (b == 0) {
+        x = 1; y = 0;
+        return a;
+    }
+    long long x1, y1;
+    long long g = extgcd(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - (a / b) * y1;
+    return g;
+}
